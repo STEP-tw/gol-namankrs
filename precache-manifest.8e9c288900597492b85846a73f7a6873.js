5c84368c11421b21f27ed6aac09872943ff93f7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 17:45:10 +0000

    Deploy STEP-tw/gol-namankrs to github.com/STEP-tw/gol-namankrs.git:gh-pages

---
+++ precache-manifest.8e9c288900597492b85846a73f7a6873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c12ec52101a0233514a3",
+    "url": "/gol-namankrs/static/js/runtime~main.c12ec521.js"
+  },
+  {
+    "revision": "8171d71d91e50cbb5da6",
+    "url": "/gol-namankrs/static/js/main.8171d71d.chunk.js"
+  },
+  {
+    "revision": "58a2f2bce9d8a3154c18",
+    "url": "/gol-namankrs/static/js/1.58a2f2bc.chunk.js"
+  },
+  {
+    "revision": "8171d71d91e50cbb5da6",
+    "url": "/gol-namankrs/static/css/main.4b7c16dc.chunk.css"
+  },
+  {
+    "revision": "e888aa43f90fc1680869d853863b16f3",
+    "url": "/gol-namankrs/index.html"
+  }
+];
