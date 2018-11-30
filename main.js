443e2b466e4265079d78698d2385bdc93fc29e48
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-11-30 12:42:35 +0530

    refactoring

--- main.js
+++ main.js
@@ -2,24 +2,15 @@ const readline = require('readLine-sync');
 const {initialiseBoard,cycleGenerator,generateBoard,
   makeBoard,generateInitialBoard,validateInputs,handleStates} = require('./src/lib.js');
 
-const generateInstances = function(length,breadth,initialStates,generation){
-  let board = generateInitialBoard(length,breadth,initialStates);
-  let resultBoard = board.map(x=>x.slice());
-
-  for(let genCount=0; genCount<generation; genCount++){
-    resultBoard = cycleGenerator(length,breadth,resultBoard);
-  }
-  return resultBoard;
-}
-
 const main = function(){
-let height = +readline.question("Enter the number of rows of the board : ");
-let width = +readline.question("Enter the number of columns of the board : ");
-let initialStates = readline.question("Enter the initial states seperated by a space e.g: 0,0 1,2 ... : ");
-let validStates = handleStates(initialStates,width,height);
-let generation = +readline.question("Enter the generation : ");
-let resultBoard = (generateInstances(width,height,validStates,generation)); 
-console.log(makeBoard(resultBoard).join("\n"));
+  let height = +readline.question("Enter the number of rows of the board : ");
+  let width = +readline.question("Enter the number of columns of the board : ");
+  let initialStates = readline.question("Enter the initial states seperated by a space e.g: 0,0 1,2 ... : ");
+  let validStates = handleStates(initialStates,width,height);
+  let generation = +readline.question("Enter the generation : ");
+  let resultBoard = generateInitialBoard(width,height,validStates);
+  resultBoard = cycleGenerator(width,height,resultBoard);
+  console.log(makeBoard(resultBoard).join("\n"));
 }
 
 main();
