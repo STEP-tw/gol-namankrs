470231404ae0e2b1133df2184498bce715f5b148
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-11-30 13:20:32 +0530

    refactoring

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ const main = function(){
   let initialStates = readline.question("Enter the initial states seperated by a space e.g: 0,0 1,2 ... : ");
   let validStates = handleStates(initialStates,width,height);
   let generation = +readline.question("Enter the generation : ");
-  let resultBoard = generateInitialBoard(width,height,validStates);
-  resultBoard = cycleGenerator(width,height,resultBoard);
-  console.log(makeBoard(resultBoard).join("\n"));
+  let initialBoard = generateInitialBoard(width,height,validStates);
+  for(let instance=0; instance<generation; instance++){
+    let resultBoard = cycleGenerator(width,height,initialBoard);
+    console.log(makeBoard(resultBoard).join("\n"));
+    console.clear();
+    initialBoard = resultBoard;
+  }
 }
 
 main();
